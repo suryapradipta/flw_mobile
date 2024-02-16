b1101633810cb4acc8da6e4b5ec33728bf2edbe3
suryapradipta.github.io <63577853+suryapradipta@users.noreply.github.com>
2024-02-16 08:48:54 +0800

    latest progress

--- lib/app/modules/main/home/home_page.dart
+++ lib/app/modules/main/home/home_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flw_mobile/app/common/custom_container.dart';
+import 'package:flw_mobile/app/utils/colors.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -6,7 +9,14 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text('HomePage')),
+      backgroundColor: kPrimary,
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(130.h),
+        child: Container(
+          height: 130.h,
+        ),
+      ),
+      body: SafeArea(child: CustomContainer(containerContent: Container())),
     );
   }
 }
--- lib/app/modules/main/search/search_page.dart
+++ lib/app/modules/main/search/search_page.dart
@@ -6,6 +6,9 @@ class SearchPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text("Search"),
+      ),
       body: Center(child: Text('SearchPage')),
     );
   }
